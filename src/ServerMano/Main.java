a1b2b6c8dafe8a8683908522ea2a6df7b10e6f7c
Vytas Gadliauskas <vytasgadliauskas@gmail.com>
2022-07-07 18:19:21 +0300

    1

---
+++ src/ServerMano/Main.java
@@ -0,0 +1,120 @@
+package ServerMano;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.*;
+
+public class Main {
+    public static final String ROOT_DIR = "src/ServerMano/resources";
+    public static final int PORT = 8888;
+
+    public static void main(String[] args) {
+        try (ServerSocket sc = new ServerSocket(PORT)) {
+            System.out.println("Server listening on "+PORT);
+            File rootDir = new File(ROOT_DIR);
+            if (!rootDir.exists()){ System.err.println("Root directory "+ROOT_DIR+" do not exist");}
+            while (true) {
+                Socket socket = sc.accept();
+                try (InputStream is = socket.getInputStream();
+                     Reader reader = new InputStreamReader(is, "UTF-8");
+                     BufferedReader bufferedReader = new BufferedReader(reader);
+                     OutputStream outputStream = socket.getOutputStream();
+                     Writer writer = new OutputStreamWriter(outputStream);
+                     BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
+                    String line;
+                    ArrayList<String> header = new ArrayList<>();
+                    while ((line = bufferedReader.readLine()) != null && !line.equals("")) {
+                        header.add(line);
+                    }
+                    String[] requestParsed = header.get(0).split(" ");
+                    if (requestParsed[0].equals("GET")) {
+                        File getFile = new File(ROOT_DIR + requestParsed[1]);
+                        String[] parentDirectories = requestParsed[1].split("/");
+                        if (getFile.isFile()) {
+                            String mimetype = Files.probeContentType(getFile.toPath());
+                            try (FileInputStream fis = new FileInputStream(getFile);
+                                 Reader r = new InputStreamReader(fis, "UTF-8");
+                                 BufferedReader br = new BufferedReader(r)) {
+                                if (mimetype != null && mimetype.split("/")[0].equals("image")) {
+                                    byte[] imageData = fis.readAllBytes();
+                                    bufferedWriter.write("HTTP/1.1 200 OK\r\n");
+                                    bufferedWriter.write("Content-Type: image/png\r\n");
+                                    bufferedWriter.write("Content-Length: "+imageData.length+"\r\n");
+                                    bufferedWriter.write("\r\n");
+                                    bufferedWriter.flush();
+                                    BufferedOutputStream bos = new BufferedOutputStream(outputStream);
+                                    bos.write(imageData);
+                                    bos.flush();
+                                    bos.close();
+                                } else {
+                                    bufferedWriter.write("HTTP/1.1 200 OK\r\n");
+                                    bufferedWriter.write("Content-Type: text/html\r\n");
+                                    bufferedWriter.write("\r\n");
+                                    String s;
+                                    while ((s = br.readLine()) != null) {
+                                        bufferedWriter.write(s + "\r\n");
+                                    }
+                                    bufferedWriter.flush();
+                                }
+                            }
+                        } else if (getFile.isDirectory()) {
+                            String parentDir = "";
+                            String fullDir = "";
+                            if (parentDirectories.length > 0) {
+                                for (int i = 1; i < parentDirectories.length; i++) {
+                                    fullDir += parentDirectories[i] + "/";
+                                    if (i < parentDirectories.length - 1) {
+                                        parentDir += parentDirectories[i] + "/";
+                                    }
+                                }
+                            }
+                            bufferedWriter.write("HTTP/1.1 200 OK\r\n");
+                            bufferedWriter.write("Content-Type: text/html\r\n");
+                            bufferedWriter.write("\r\n");
+                            bufferedWriter.write("<html><body>");
+                            File[] files = getFile.listFiles();
+                            bufferedWriter.write("<h1>Direktorijos turinys:</h1><pre>\r\n");
+                            bufferedWriter.write("<a href=http://localhost:8888/" + parentDir + "> <-Atgal </a> \r\n");
+                            for (int i = 0; i < files.length; i++) {
+                                if (files[i].isDirectory()) {
+                                    bufferedWriter.write("DIR <a href=http://localhost:8888/" + fullDir + files[i].getName() + ">" + files[i].getName() + "</a> \r\n");
+                                } else {
+                                    bufferedWriter.write("<a href=http://localhost:8888/" + fullDir + files[i].getName() + ">" + files[i].getName() + "</a> \r\n");
+                                }
+                            }
+                            bufferedWriter.write("</pre></body></html> \r\n");
+                            bufferedWriter.flush();
+                        } else if (!getFile.exists()) {
+                            bufferedWriter.write("HTTP/1.1 404 Not Found\r\n");
+                            bufferedWriter.write("Server: Java MyServer\r\n");
+                            bufferedWriter.write("Content-Type: text/html\r\n");
+                            bufferedWriter.write("Connection: Closed \r\n");
+                            bufferedWriter.write("\r\n");
+                            bufferedWriter.flush();
+                        }
+                    } else if (requestParsed[0].equals("POST")){
+                        for (int i = 0; i < header.size(); i++) {
+                            System.out.println(header.get(i));
+                        }
+
+
+                    } else {
+                        bufferedWriter.write("HTTP/1.1 400 Bad Request\r\n");
+                        bufferedWriter.write("Server: Java MyServer\r\n");
+                        bufferedWriter.write("Content-Type: text/html\r\n");
+                        bufferedWriter.write("Connection: Closed \r\n");
+                        bufferedWriter.write("\r\n");
+                        bufferedWriter.flush();
+                    }
+                } catch (IOException ex) {
+                    System.out.println("Klaida iostream " + ex.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Klaida port " + e.getMessage());
+        }
+    }
+
+}
